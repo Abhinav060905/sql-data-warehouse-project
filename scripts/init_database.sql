b96d48c36802597fc631615ca8cb47ba187be1ae
Abhinav Bharat Kaushik <ak123@snu.edu.in>
2026-09-10 19:47:41 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
+-- Switch to the master database
+USE MASTER;
+GO
 
+-- Create the Data Warehouse database
+CREATE DATABASE DataWarehouse;
+GO
+
+-- Switch to the newly created DataWarehouse database
+USE DataWarehouse;
+GO
+
+-- Create the Bronze schema
+-- Used for storing raw data extracted from source systems
+CREATE SCHEMA bronze;
+GO
+
+-- Create the Silver schema
+-- Used for cleaned and transformed data
+CREATE SCHEMA silver;
+GO
+
+-- Create the Gold schema
+-- Used for business-ready and analytical data
+CREATE SCHEMA gold;
+GO
